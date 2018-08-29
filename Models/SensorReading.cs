d75707f4e8d1d0de933e2af532450f3c44683d86
Mihai Costea <macostea@users.noreply.github.com>
2018-08-29 19:08:49 +0300

    Use react and API controllers

---
+++ Models/SensorReading.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Common;
+
+namespace HomeManager.Models
+{
+    public class SensorReading<T> : ISensorReading<T>
+    {
+        public DateTime Time { get; }
+        public T Reading { get; }
+
+        public SensorReading(DateTime time, T reading)
+        {
+            this.Time = time;
+            this.Reading = reading;
+        }
+
+        public override string ToString()
+        {
+            return $"Time: {this.Time}, Reading: {this.Reading}";
+        }
+    }
+}

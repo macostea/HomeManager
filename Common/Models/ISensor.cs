566853775b23baedaffe91f234780c21712fd755
Mihai Costea <macostea@users.noreply.github.com>
2018-09-03 18:02:50 +0300

    [SensorListener] Added repo and sensor support

--- Common/Models/ISensor.cs
+++ Common/Models/ISensor.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
 namespace HomeManager.Common.Models
 {
-    public interface ISensor
+    public interface ISensor<T>
     {
+        IEnumerable<ISensorReading<T>> GetLatestReadings();
+        IEnumerable<ISensorReading<T>> GetReadings();
+        IEnumerable<ISensorReading<T>> GetReadings(Predicate<ISensorReading<T>> filter);
+
+        Task<bool> SaveReading(ISensorReading<T> reading);
     }
 }

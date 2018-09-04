42da58c6f8c216c8f39ab7da813d0b7f4f791ba2
Mihai Costea <mihai.costea@telenav.com>
2018-09-04 18:05:16 +0300

    [WIP]Added SaveToDB function

---
+++ Functions/SaveToDB/FunctionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using HomeManager.Common.Repository;
+using HomeManager.Common.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public async Task<string> Handle(string input) {
+            var inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(input);
+
+            var dBContext = new TimescaleDBContext();
+            var repository = new TempSensorReadingsRepository(dBContext);
+
+
+            switch (inputDictionary["type"])
+            {
+                case "temperature":
+                    var reading = JsonConvert.DeserializeObject<SensorReading<double>>(inputDictionary["reading"]);
+                    var location = inputDictionary["location"];
+                    var sensor = new TempSensor(location, repository);
+
+                    return await sensor.SaveReading(reading) ? "worked" : "didn't work";
+                    break;
+            }
+            return "";
+        }
+    }
+}

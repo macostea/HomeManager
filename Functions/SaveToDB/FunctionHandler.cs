8a92d748d0c5a870eb1b1a99a589428a6640f2cd
Mihai Costea <macostea@users.noreply.github.com>
2018-09-06 15:58:10 +0300

    [FUN][WIP]Added SaveToDB function

--- Functions/SaveToDB/FunctionHandler.cs
+++ Functions/SaveToDB/FunctionHandler.cs
@@ -11,7 +11,7 @@ namespace Function
     public class FunctionHandler
     {
         public async Task<string> Handle(string input) {
-            var inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(input);
+            var inputDictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(input);
 
             var dBContext = new TimescaleDBContext();
             var repository = new TempSensorReadingsRepository(dBContext);
@@ -20,9 +20,9 @@ namespace Function
             switch (inputDictionary["type"])
             {
                 case "temperature":
-                    var reading = JsonConvert.DeserializeObject<SensorReading<double>>(inputDictionary["reading"]);
+                    var reading = JsonConvert.DeserializeObject<SensorReading<double>>(inputDictionary["reading"].ToString());
                     var location = inputDictionary["location"];
-                    var sensor = new TempSensor(location, repository);
+                    var sensor = new TempSensor(location.ToString(), repository);
 
                     return await sensor.SaveReading(reading) ? "worked" : "didn't work";
                     break;
---
+++ Functions/SaveToDB/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Function;
+
+namespace SaveToDB
+{
+    public static class Program
+    {
+        private static string GetStdin()
+        {
+            StringBuilder buffer = new StringBuilder();
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                buffer.AppendLine(s);
+            }
+            return buffer.ToString();
+        }
+
+        static async Task Main(string[] args)
+        {
+            string buffer = GetStdin();
+            FunctionHandler f = new FunctionHandler();
+
+            string responseValue = await f.Handle(buffer);
+
+            if (responseValue != null)
+            {
+                Console.Write(responseValue);
+            }
+        }
+    }
+}

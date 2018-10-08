02ebb75b7084446e4b30851d088aa778346ad375
Mihai Costea <mihai.andrei.costea@icloud.com>
2018-10-08 19:54:21 +0300

    Added tests; [WIP] Fix failing build for other projects

--- SensorServiceTests/SensorsControllerTest.cs
+++ SensorServiceTests/SensorsControllerTest.cs
@@ -5,6 +5,7 @@ using Common.Models;
 using Common.Repository;
 using Moq;
 using SensorService.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace SensorServiceTests
@@ -69,6 +70,113 @@ namespace SensorServiceTests
 
             Assert.NotNull(result);
             Assert.Equal(result, sensors[1]);
+        }
+
+        [Fact]
+        public async Task Post_WhenCalled_ReturnsOkResult()
+        {
+            var mockedRepo = new Mock<IRepository<Sensor>>();
+
+            var sensors = new List<Sensor>
+            {
+                new Sensor()
+                {
+                    Name = "test_sensor_1",
+                    SensorId = 1
+                },
+                new Sensor()
+                {
+                    Name = "test_sensor_2",
+                    SensorId = 2
+                }
+            };
+
+            mockedRepo.Setup(repo => repo.GetById(1)).ReturnsAsync(sensors[0]);
+            mockedRepo.Setup(repo => repo.GetById(2)).ReturnsAsync(sensors[1]);
+
+            var controller = new SensorsController(mockedRepo.Object);
+
+            var newSensor = new Sensor()
+            {
+                Name = "test_sensor_1",
+                SensorId = 3
+            };
+
+            mockedRepo.Setup(repo => repo.Add(newSensor)).ReturnsAsync(true);
+            mockedRepo.Setup(repo => repo.GetById(3)).ReturnsAsync(newSensor);
+
+            var result = await controller.Post(newSensor);
+            var contentResult = result as CreatedAtRouteResult;
+
+            Assert.NotNull(contentResult);
+            Assert.NotNull(contentResult.Value);
+            Assert.Equal(contentResult.Value, newSensor);
+        }
+
+        [Fact]
+        public async Task Post_WhenCalled_BadObject_ReturnsBadRequest()
+        {
+            var mockedRepo = new Mock<IRepository<Sensor>>();
+
+            var controller = new SensorsController(mockedRepo.Object);
+
+            var newSensor = new Sensor()
+            {
+                Name = "test_sensor_1",
+                SensorId = 3
+            };
+
+            mockedRepo.Setup(repo => repo.Add(newSensor)).ReturnsAsync(false);
+
+            var result = await controller.Post(newSensor);
+            var contentResult = result as BadRequestResult;
+
+            Assert.NotNull(contentResult);
+        }
+
+        [Fact]
+        public async Task Put_WhenCalled_ReturnsOk()
+        {
+            var mockedRepo = new Mock<IRepository<Sensor>>();
+
+            var controller = new SensorsController(mockedRepo.Object);
+
+            var newSensor = new Sensor()
+            {
+                Name = "test_sensor_1",
+                SensorId = 3
+            };
+
+            mockedRepo.Setup(repo => repo.Edit(newSensor)).ReturnsAsync(true);
+
+            var result = await controller.Put(newSensor);
+            var contentResult = result as OkObjectResult;
+
+            Assert.NotNull(contentResult);
+            Assert.NotNull(contentResult.Value);
+            Assert.Equal(contentResult.Value, newSensor);
+        }
+
+        [Fact]
+        public async Task Put_WhenCalled_BadObject_ReturnsBadRequest()
+        {
+            var mockedRepo = new Mock<IRepository<Sensor>>();
+
+            var controller = new SensorsController(mockedRepo.Object);
+
+            var newSensor = new Sensor()
+            {
+                Name = "test_sensor_1",
+                SensorId = 3
+            };
+
+            mockedRepo.Setup(repo => repo.Edit(newSensor)).ReturnsAsync(false);
+
+            var result = await controller.Put(newSensor);
+            var contentResult = result as BadRequestResult;
+
+            Assert.NotNull(contentResult);
+
         }
     }
 }

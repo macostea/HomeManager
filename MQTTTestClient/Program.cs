f273965af05c471876fb2da1d1b95a356ae2879b
Mihai Costea <macostea@users.noreply.github.com>
2018-10-23 16:34:10 +0300

    Update MQTT Test Client

--- MQTTTestClient/Program.cs
+++ MQTTTestClient/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Mqtt;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MQTTTestClient
 {
@@ -10,10 +12,17 @@ namespace MQTTTestClient
         static async Task Main(string[] args)
         {
             var configuration = new MqttConfiguration();
-            var client = await MqttClient.CreateAsync("10.230.5.60", configuration);
+            var client = await MqttClient.CreateAsync("foxey-lady-master.mcostea.com", configuration);
             var sessionState = await client.ConnectAsync();
 
-            var message = new MqttApplicationMessage("donnowhat", Encoding.UTF8.GetBytes("Hello world"));
+            var tempReading = new Dictionary<string, object>
+            {
+                { "sensorId", 2 },
+                { "time", "2018-10-23T13:14:17Z"},
+                { "reading", 21.0 }
+            };
+
+            var message = new MqttApplicationMessage("temperature", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(tempReading)));
             await client.PublishAsync(message, MqttQualityOfService.AtMostOnce);
 
             await client.DisconnectAsync();

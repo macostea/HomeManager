8b9a9d746f7b486569d0b9d86d7960c3e50e560a
Mihai Costea <macostea@users.noreply.github.com>
2018-10-23 16:10:21 +0300

    Added rabbitmq config file and MQTT Test Client

---
+++ MQTTTestClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Mqtt;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTTTestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new MqttConfiguration();
+            var client = await MqttClient.CreateAsync("10.230.5.60", configuration);
+            var sessionState = await client.ConnectAsync();
+
+            var message = new MqttApplicationMessage("donnowhat", Encoding.UTF8.GetBytes("Hello world"));
+            await client.PublishAsync(message, MqttQualityOfService.AtMostOnce);
+
+            await client.DisconnectAsync();
+        }
+    }
+}

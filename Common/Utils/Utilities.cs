ae4462077cce5131807a7bf1c24d01ff2be5f88c
Mihai Costea <mihai.andrei.costea@icloud.com>
2019-01-06 19:08:59 +0200

    Added Utilities class

---
+++ Common/Utils/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Utils
+{
+    public static class Utilities
+    {
+        public static async Task<T> ExecuteRestRequest<T>(Uri BaseURL, RestRequest request) where T : new()
+        {
+            var client = new RestClient
+            {
+                BaseUrl = BaseURL
+            };
+            var response = await client.ExecuteTaskAsync<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                const string message = "Error retrieving response.  Check inner details for more info.";
+                var exception = new ApplicationException(message, response.ErrorException);
+                throw exception;
+            }
+            return response.Data;
+        }
+    }
+}

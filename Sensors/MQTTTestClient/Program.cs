cb13dfa73de78db85ecfa58d58d3bb16acc45664
Mihai Costea <mihai.andrei.costea@icloud.com>
2019-01-06 20:47:16 +0200

    Added WeatherSensor Docker support

---
+++ Sensors/MQTTTestClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mqtt;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MQTTTestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new MqttConfiguration();
+            var client = await MqttClient.CreateAsync("foxey-lady-master.mcostea.com", configuration);
+            var sessionState = await client.ConnectAsync();
+
+            var tempReading = new Dictionary<string, object>
+            {
+                { "sensorId", 2 },
+                { "time", "2018-10-23T13:14:17Z"},
+                { "reading", 21.0 }
+            };
+
+            var message = new MqttApplicationMessage("temperature", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(tempReading)));
+            await client.PublishAsync(message, MqttQualityOfService.AtMostOnce);
+
+            await client.DisconnectAsync();
+        }
+    }
+}

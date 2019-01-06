ef9949d13ca3ee2c6f540135a9200738763425e4
Mihai Costea <mihai.andrei.costea@icloud.com>
2019-01-06 20:47:16 +0200

    Added WeatherSensor Docker support

---
+++ Sensors/WeatherSensor/Client/OpenWeatherMapClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Common.Utils;
+using RestSharp;
+
+namespace WeatherSensor.Client
+{
+    public class OpenWeatherMapResponseObject
+    {
+        public List<Dictionary<string, object>> Weather { get; set; }
+        public Dictionary<string, object> Main { get; set; }
+    }
+
+    class OpenWeatherMapClient : IWeatherClient
+    {
+        public Uri BaseURL { get; }
+        public string APIVersion { get; }
+        public string APIKey { get; }
+        public string CityName { get; }
+        public OpenWeatherMapClient(string cityName, string apiKey)
+        {
+            this.BaseURL = new Uri("https://api.openweathermap.org/data");
+            this.APIVersion = "2.5";
+            this.CityName = cityName;
+            this.APIKey = apiKey;
+        }
+
+        public async Task<WeatherSensorReading> GetCurrentConditions()
+        {
+            var request = new RestRequest
+            {
+                Resource = $"{this.APIVersion}/weather"
+            };
+
+            request.AddParameter("q", this.CityName);
+            request.AddParameter("appid", this.APIKey);
+            request.AddParameter("units", "metric");
+            
+            request.Method = Method.GET;
+
+            var conditions = await Utilities.ExecuteRestRequest<OpenWeatherMapResponseObject>(this.BaseURL, request);
+            var sensorReading = new WeatherSensorReading()
+            {
+                Temperature = Convert.ToDouble(conditions.Main["temp"]),
+                Pressure = Convert.ToDouble(conditions.Main["pressure"]),
+                Humidity = Convert.ToDouble(conditions.Main["humidity"]),
+                MinimumTemperature = Convert.ToDouble(conditions.Main["temp_min"]),
+                MaximumTemperature = Convert.ToDouble(conditions.Main["temp_max"]),
+                ConditionCode = Convert.ToInt32(conditions.Weather[0]["id"]),
+                Condition = (string)conditions.Weather[0]["main"],
+                IconURL = new Uri(new Uri("http://openweathermap.org/img/w/"), (string)conditions.Weather[0]["icon"])
+            };
+
+            return sensorReading;
+        }
+    }
+}
---
+++ Sensors/WeatherSensor/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Mqtt;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherSensor.Client;
+
+namespace WeatherSensor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var openweathermapAppid = Environment.GetEnvironmentVariable("OPENWEATHERMAP_APPID");
+            if (openweathermapAppid == null)
+            {
+                openweathermapAppid = await System.IO.File.ReadAllTextAsync("/run/secrets/OPENWEATHERMAP_APPID");
+            }
+
+            var client = new OpenWeatherMapClient(
+                Environment.GetEnvironmentVariable("WEATHER_LOCATION"),
+                openweathermapAppid
+            );
+            var response = await client.GetCurrentConditions();
+            response.SensorId = Convert.ToInt32(Environment.GetEnvironmentVariable("SENSOR_ID"));
+
+            var configuration = new MqttConfiguration();
+            var mqttClient = await MqttClient.CreateAsync(Environment.GetEnvironmentVariable("RABBITMQ_HOST"), configuration);
+            var sessionState = await mqttClient.ConnectAsync();
+
+            var message = new MqttApplicationMessage("weather", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response)));
+            await mqttClient.PublishAsync(message, MqttQualityOfService.AtMostOnce);
+
+            await mqttClient.DisconnectAsync();
+        }
+    }
+}

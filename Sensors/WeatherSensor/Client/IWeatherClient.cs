606cd872d3f9a5ed1f56d34474361c59711fe9fa
Mihai Costea <macostea@users.noreply.github.com>
2019-01-06 20:47:16 +0200

    Added WeatherSensor Docker support

---
+++ Sensors/WeatherSensor/Client/IWeatherClient.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherSensor.Client
+{
+    interface IWeatherClient
+    {
+        Task<WeatherSensorReading> GetCurrentConditions();
+    }
+}

841378c830b9b0be12a9ca9b554bde0498919340
Mihai Costea <macostea@users.noreply.github.com>
2019-01-06 18:49:14 +0200

    Added WeatherSensorListener

---
+++ SensorListener/Listeners/WeatherSensorListener.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace SensorListener.Listeners
+{
+    class WeatherSensorListener : SensorReadingListener
+    {
+        public WeatherSensorListener(string sensorServiceURL) : base("weather", sensorServiceURL) {}
+
+        public override async Task<bool> ProcessMessageAsync(string message)
+        {
+            try
+            {
+                var reading = JsonConvert.DeserializeObject<WeatherSensorReading>(message);
+                reading.Time = DateTime.UtcNow;
+
+                var result = await this.Client.CreateWeatherReading(reading);
+
+                Console.WriteLine(result);
+                return true;
+            }
+            catch (ApplicationException e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}

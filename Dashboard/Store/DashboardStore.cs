08affc860e9f19c62882e84a736dd7c21e1edb70
Mihai Costea <macostea@users.noreply.github.com>
2019-05-23 16:33:08 +0300

    Updated .net core version and blazor version

--- Dashboard/Store/DashboardStore.cs
+++ Dashboard/Store/DashboardStore.cs
@@ -1,6 +1,5 @@
 ﻿using Domain.Entities;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;

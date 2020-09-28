26702c5e5c4fa55c6ff42981373d5e063e5d4429
Mihai Costea <macostea@users.noreply.github.com>
2020-09-28 11:08:31 +0300

    Fix return values of ArduinoHomeyClient methods

--- Sensors/ESPTempSensor/src/arduino_homey_client.cpp
+++ Sensors/ESPTempSensor/src/arduino_homey_client.cpp
@@ -12,16 +12,18 @@ ArduinoHomeyClient::~ArduinoHomeyClient() {
 bool ArduinoHomeyClient::begin(std::string id) {
     this->client->begin(String(id.c_str()));
     this->client->setClass("sensor");
-    this->client->addCapability("measure_humidity");
-    this->client->addCapability("measure_temperature");
+    bool humRes = this->client->addCapability("measure_humidity");
+    bool tempRes = this->client->addCapability("measure_temperature");
+
+    return humRes && tempRes;
 }
 
 bool ArduinoHomeyClient::updateTemperature(double temperature) {
-    this->client->setCapabilityValue("measure_temperature", temperature);
+    return this->client->setCapabilityValue("measure_temperature", temperature);
 }
 bool ArduinoHomeyClient::updateHumidity(double humidity) {
-    this->client->setCapabilityValue("measure_humidity", humidity);
+    return this->client->setCapabilityValue("measure_humidity", humidity);
 }
 bool ArduinoHomeyClient::loop() {
-    this->client->loop();
+    return this->client->loop();
 }

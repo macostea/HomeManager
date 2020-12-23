c2b2c795bdf9d0179b827d18061b000c999388ce
Mihai Costea <macostea@users.noreply.github.com>
2020-12-23 12:30:47 +0200

    Added initial fetch data route for Index

--- BlazorDashboard/Program.cs
+++ BlazorDashboard/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BlazorDashboard.Services;
 
 namespace BlazorDashboard
 {
@@ -17,7 +18,8 @@ namespace BlazorDashboard
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://sensor-service.mcostea.com/api") });
+            builder.Services.AddSingleton(sp => new SensorServiceClient(new HttpClient { BaseAddress = new Uri("https://sensor-service.mcostea.com/api") }));
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorDashboard/Services/SensorServiceClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace BlazorDashboard.Services
+{
+    public class SensorServiceClient
+    {
+        public HttpClient httpClient { get; set; }
+
+        public SensorServiceClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<Home>> GetHomes()
+        {
+            return await this.httpClient.GetFromJsonAsync<List<Home>>("homes");
+        }
+
+        public async Task<List<Room>> GetRooms(Home home)
+        {
+            return await this.httpClient.GetFromJsonAsync<List<Room>>("homes/" + home.Id.ToString() + "/room");
+        }
+
+        //public async Task<List<Weather>> GetWeatherToday()
+        //{
+
+        //}
+    }
+}
